f732bd87169983c6d9498ea6a79060a7578be692
Abhishek singh <singhabhi965133@gmail.com>
2026-06-21 19:40:06 +0530

    Changes

--- frontend/src/utils/authService.jsx
+++ frontend/src/utils/authService.jsx
@@ -1,24 +1,36 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 const authService = {
   signup: async (userData) => {
-    const res = await axios.post("/auth/signup", userData, { withCredentials: true });
+    const res = await axios.post(`${API_URL}/auth/signup`, userData, {
+      withCredentials: true,
+    });
     return res.data;
   },
 
   login: async (credentials) => {
-    const res = await axios.post("/auth/login", credentials, { withCredentials: true });
+    const res = await axios.post(`${API_URL}/auth/login`, credentials, {
+      withCredentials: true,
+    });
     return res.data;
   },
 
   logout: async () => {
-    const res = await axios.post("/auth/logout", {}, { withCredentials: true });
+    const res = await axios.post(
+      `${API_URL}/auth/logout`,
+      {},
+      { withCredentials: true }
+    );
     return res.data;
   },
 
   getCurrentUser: async () => {
     try {
-      const res = await axios.get("/auth/user", { withCredentials: true });
+      const res = await axios.get(`${API_URL}/auth/user`, {
+        withCredentials: true,
+      });
       return res.data;
     } catch {
       return { loggedIn: false };
